3892fdbdb209dfb90f2074d1fbadde73a536fff4
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2022-04-05 10:41:11 -0700

    ..

--- EmptyTreeException.java
+++ EmptyTreeException.java
@@ -1,1 +1,13 @@
+public class EmptyTreeException extends RuntimeException
+{
+	public EmptyTreeException()
+	{
+		this(null);
+	} // end default constructor
+
+	public EmptyTreeException(String message)
+	{
+		super(message);
+	} // end constructor
+} // end EmptyTreeException
 
